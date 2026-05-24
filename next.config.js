69d6dcfa238a91df566b9238cae79fd8c5f74823
John <john4button@gmail.com>
2026-05-24 02:44:01 -0700

    depcreate this shie, we moving to portfolio2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://www.john-liu.dev/:path*",
+        permanent: true,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
